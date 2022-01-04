1559e1c13acdd998fa86ccc78861736843936bbf
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-04 18:15:43 +0500

    react router and pages made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
+      <About/>
+      <Projects/>
+      <Blog/>
     </div>
   );
 }
--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -2,22 +2,32 @@ import styled from "styled-components";
 
 export const Wrapper = styled.ul`
   display: flex;
-  gap:50px;
+  align-items: center;
+  width: 100%;
+  gap: 50px;
   list-style-type: none;
   margin-left: 100px;
+  margin-top: 25px;
 `;
 
 export const Header = styled.nav`
-display: flex;
-align-items: center;
-`
-
-export const Nav = styled.li`
- font-size: 1.125rem;
- align-items: center;
- cursor: pointer;
+  display: flex;
+  align-items: center;
+  height: 50px;
+  /* justify-content: space-evenly; */
 `;
 
-export const Brand = styled.h2`
- margin-bottom:5px;
-`
+export const Nav = styled.li`
+  font-size: 1.125rem;
+  align-items: center;
+  cursor: pointer;
+  padding: 4px 15px;
+  &:hover {
+    background-color:#e8edea;
+    border-radius: 70px;
+  }
+`;
+
+export const Brand = styled.div`
+  font-size: 1.5rem;
+`;
