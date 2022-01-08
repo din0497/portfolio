d2da29ec24950cda274cffe1e96af7294536660f
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-08 16:10:57 +0500

    paragraphs

---
+++ src/commons/Title.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Title = styled.p`
+color: rgb(20 184 166);
+font-weight:500;
+`
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,30 @@
+import { Title } from "../../commons/Title";
+import { AboutContainer } from "./style";
+
 const About = () => {
-    return <div>
-      
-    </div>;
-  };
-  
-  export default About
+  return (
+    <AboutContainer>
+      <Title>ABOUT ME</Title>
+      <h1>Here`s my story</h1>
+      <p style={{ maxWidth: "887px", textAlign: "start", color: 'gray' }}>
+        I’m Braydon, a developer, creative coder, blogger and self-proclaimed
+        designer who specializes in front-end development. My mission is to
+        translate user-focussed designs into pixel-perfect websites or
+        applications that run blazing fast.
+      </p>
+      {/* <p>
+        I’m currently working as a senior full-stack engineer at Cognizant where
+        I help architect and develop full-stack RESTful microservices, train and
+        prepare developers for delivery, and assist in leading the front-end
+        practice in a lab-based working environment.
+      </p>
+      <p>
+        Before Cognizant, I worked as a UI Developer for projekt202 helping
+        craft design systems and building reusable component libraries for
+        multi-million dollar companies.
+      </p> */}
+    </AboutContainer>
+  );
+};
+
+export default About;
