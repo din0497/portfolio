90209520a41520d4180351adf261127e4fdff5b6
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-10 23:03:35 +0500

    I write about my self

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,15 @@
 import { Title } from "../../commons/Title";
-import { AboutContainer, Article, ExperienceSection, MeSection, Paragraph } from "./style";
+import {
+  AboutContainer,
+  Article,
+  ExperienceSection,
+  MeSection,
+  Paragraph,
+} from "./style";
 import me from "../../assets/me.jpg";
 import { Me } from "./style";
 import RecentBlogs from "../../commons/RecentBlogs";
 import Experience from "./Experince";
-
 
 const About = () => {
   return (
@@ -13,35 +18,36 @@ const About = () => {
       <Article>
         <h1>Here`s my story</h1>
         <Paragraph>
-          I’m Braydon, a developer, creative coder, blogger and self-proclaimed
-          designer who specializes in front-end development. My mission is to
-          translate user-focussed designs into pixel-perfect websites or
-          applications that run blazing fast.
+          I`m Rivojiddin, a developer, creative coder, blogger and
+          self-proclaimed designer who specializes in frontend development. My
+          mission is to translate user-focussed designs into pixel-perfect
+          websites that run blazing fast.
         </Paragraph>
         <Paragraph>
-          I’m currently working as a senior full-stack engineer at Cognizant
-          where I help architect and develop full-stack RESTful microservices,
-          train and prepare developers for delivery, and assist in leading the
+          I`m currently working as a frontend engineer at Genius Id where I help
+          architect,develop and test frontend parts and assist in leading the
           front-end practice in a lab-based working environment.
         </Paragraph>
         <Paragraph>
-          Before Cognizant, I worked as a UI Developer for projekt202 helping
-          craft design systems and building reusable component libraries for
-          multi-million dollar companies.
+          Before Genius Id, I freelanced as a Frontend developer for Borbaraka
+          helping people to ship cargos from South Korea to centeral Asia.
         </Paragraph>
       </Article>
       <MeSection>
         <Me src={me} />
         <Article>
           <Paragraph>
-            In a past life, I was an indie mobile developer making mobile games
-            for iOS and Android, with one of my games outselling Angry Birds.
+            Currently, I am a senior student at Sejong University in South
+            Korea. I like to code things from scratch, and enjoy bringing ideas
+            to life in the browser.
           </Paragraph>
           <Paragraph>
-            You can find me on Twitter where I share tech-related tidbits and
-            build in public, or you can follow me on GitHub. I often write about
-            my findings on my blog and create cool things over on CodePen. I
-            also help run a mediocre podcast.
+            Since beginning my journey as a web developer nearly a year ago,
+            I've done remote work for small agencies, consulted for small-scale
+            startups, and collaborated with talented people to create digital
+            products for both business and consumer use. I'm quietly confident,
+            naturally curious, and perpetually working on improving my chops one
+            problem at a time.
           </Paragraph>
         </Article>
       </MeSection>
