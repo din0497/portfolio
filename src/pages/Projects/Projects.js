d397413f87dbf9efe6318f6ffdd0fc85f6a94616
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-11 21:26:06 +0500

    Descriptions

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
         <Description
           link="https://loadfetcher.com/"
           title="LoadFetcher"
-          text="Pomegradient is a web-based gradient solutiuon currently in beta that allows you to find, save and craft gradients with a creative community!"
+          text="LoadFetcher is a multi loadboard search platform. It merges multiple loadboards and makes it easy to see all the loads from different loadboards without logging in each of the loadboards."
         />
         <Back>
           <Img src={loadfetcher} />
@@ -24,7 +24,10 @@ const Projects = () => {
         <Description
           link="https://borbaraka.netlify.app/"
           title="Borbaraka"
-          text="Pomegradient is a web-based gradient solutiuon currently in beta that allows you to find, save and craft gradients with a creative community!"
+          text="BorBaraka is Korean Company located in Seoul. I built
+           this web site with using React, Styled
+          Components, React Router. Teck stack: TailwindCss,
+          using React, Styled Components, React Router."
         />
         <Back>
           <Img src={project} />
@@ -34,7 +37,7 @@ const Projects = () => {
         <Description
           link="https://bill-hotel-site.netlify.app/"
           title="Bill Hotel"
-          text="Pomegradient is a web-based gradient solutiuon currently in beta that allows you to find, save and craft gradients with a creative community!"
+          text="Bill Hotel is hotel reservetion app for booking, reserving and browsing hotel rooms. I built it using Ract, React hooks, CSS "
         />
         <Back>
           <Img src={hotel2} />
