2ad0f2e37475b12d430e11b5cd22b93b881bf22b
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-14 03:46:29 +0500

    /*    /index.html    200

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,18 +1,19 @@
-// import { Title } from "../../commons/Title";
-// import { blogs } from "../Home/Home";
+import { Title } from "../../commons/Title";
+import { blogs } from "../Home/Home";
 import { BlogGrid } from "./style";
-// import Blog from "../../commons/Blog";
+import Blog from "../../commons/Blog";
 
 const Blogs = () => {
-  return (
-    <BlogGrid>
-      {/* {blogs.map((blog, i) => {
-        return (
-          <Blog key={i} img={blog.img} title={blog.title} date={blog.date} />
-        );
-      })} */}
-    </BlogGrid>
-  );
+  return <Title>BLOG</Title>
+  // (
+  //   <BlogGrid>
+  //     {blogs.map((blog, i) => {
+  //       return (
+  //         <Blog key={i} img={blog.img} title={blog.title} date={blog.date} />
+  //       );
+  //     })}
+  //   </BlogGrid>
+  // );
 };
 
 export default Blogs;
