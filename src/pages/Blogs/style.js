b846d18b8061188099fb24af39279ef5cf541506
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-14 23:47:21 +0500

    Create post

--- src/pages/Blogs/style.js
+++ src/pages/Blogs/style.js
@@ -1,20 +1,19 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const BlogGrid = styled.div`
- display: grid;
- grid-template-columns: 30% 30% 30%;
- row-gap:20px;
- column-gap:20px;
- justify-content: center;
- margin-top: 30px;
- @media only screen and (max-width: 800px) {
-    grid-template-columns: 45% 45%; 
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+  row-gap: 20px;
+  column-gap: 20px;
+  justify-content: center;
+  margin-top: 30px;
+  @media only screen and (max-width: 800px) {
+    grid-template-columns: 45% 45%;
   }
   @media only screen and (max-width: 436px) {
-    grid-template-columns: 90%; 
+    grid-template-columns: 90%;
   }
-`
+`;
 
 export const BlogCart = styled.div`
   display: flex;
@@ -24,27 +23,38 @@ export const BlogCart = styled.div`
 
 export const CreateContainer = styled.div`
   width: 90%;
-  color: rgba(51,51,51);
-  background-color: rgba(245,247,250);
-  border: rgba(229,231,235) solid 1px;
+  color: rgba(51, 51, 51);
+  background-color: rgba(245, 247, 250);
+  border: rgba(229, 231, 235) solid 1px;
   justify-content: center;
   display: flex;
   align-items: center;
   border-radius: 15px;
   cursor: pointer;
- 
-`
+`;
 
 export const Posting = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Textarea = styled.textarea`
-`
-export const Addons = styled.div``
+  color: rgba(51, 51, 51);
+  line-height: 1.375;
+  font-weight: 700;
+  font-size: 1.875rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  background-color: transparent;
+  appearance: none;
+  resize: none;
+  width: 100%;
+  margin-bottom: 1.25rem;
+  margin-top: 0.5rem;
+  /* outline:none; */
+  /* border:none; */
+}
+`;
+export const Addons = styled.div``;
 
-export const AddonsContainer = styled.div``
-
-
-
+export const AddonsContainer = styled.div``;
