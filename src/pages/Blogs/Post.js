699c9d51ac28712973f04cc4ca6304dead6241e9
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-18 23:57:33 +0500

    spiner

--- src/pages/Blogs/Post.js
+++ src/pages/Blogs/Post.js
@@ -4,6 +4,7 @@ import { AboutContainer, Article, Paragraph } from "../About/style";
 import { PostImg, PostTitle } from "./style";
 import useHttp from "../../hooks/use-http";
 import { getSingleBlog } from "../../lib/api";
+import BeatLoader from "react-spinners/BeatLoader";
 
 const Post = () => {
   const params = useParams();
@@ -16,9 +17,9 @@ const Post = () => {
     sendRequest(id);
   }, [sendRequest, id]);
 
-  if (status === "pending") {
-    return <p>loading...</p>;
-  }
+  // if (status === "pending") {
+  //   return <p>loading...</p>;
+  // }
 
   if (error) {
     return <p>{error}</p>;
@@ -30,7 +31,7 @@ const Post = () => {
 
   return (
     <AboutContainer>
-      <PostTitle>{blog.title}</PostTitle>
+      {/* <PostTitle>{blog.title}</PostTitle>
       <span
         style={{
           marginTop: "-40px",
@@ -46,7 +47,10 @@ const Post = () => {
         {blog.text.map((p, i) => {
           return <Paragraph key={i}>{p}</Paragraph>;
         })}
-      </Article>
+      </Article> */}
+      <div style={{ padding: "100px 0 205px  0" }}>
+        <BeatLoader color="rgb(20 184 166)" />
+      </div>
     </AboutContainer>
   );
 };
