13dc504114fb768447ac3a33ce763d76a0b5b2c1
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-18 23:30:06 +0500

    skelton

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -24,7 +24,7 @@ const Blogs = () => {
     >
       <Title>BLOG</Title>
       <CreateBlog />
-      {git ? (
+      {status === "pending" ? (
         <SkeltonGrid>
           <BlogSk />
           <BlogSk />
