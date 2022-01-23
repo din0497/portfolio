f9cdbe2b878c8d4721d007caf13753f5aad8d719
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-24 02:03:14 +0500

    auth

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,10 +1,17 @@
 import DropButton from "./DropButton";
 import { DropContainer, DropUl } from "./style";
+import { useContext } from "react";
+import AuthContext from "../../store/auth-contex";
 
 const Dropdown = ({ setDrop }) => {
+  const authCtx = useContext(AuthContext);
   const clickHandler = () => {
     setDrop("hidden");
     document.body.style.overflow = "visible";
+  };
+  const logoutHandler = () => {
+    authCtx.logout();
+    clickHandler();
   };
   return (
     <DropContainer onClick={clickHandler}>
@@ -13,6 +20,11 @@ const Dropdown = ({ setDrop }) => {
         <DropButton onClick={clickHandler} to="/about" text="About" />
         <DropButton onClick={clickHandler} to="/projects" text="Projects" />
         <DropButton onClick={clickHandler} to="/blog" text="Blog" />
+        {!authCtx.isLoggedIn ? (
+          <DropButton onClick={clickHandler} to="/auth" text="Log in" />
+        ) : (
+          <button onClick={logoutHandler}>Log out</button>
+        )}
       </DropUl>
     </DropContainer>
   );
