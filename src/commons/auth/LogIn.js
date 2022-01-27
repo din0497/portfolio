10abeccb0b6e09363ee19e03b77dad6fbbffe8a2
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-27 09:53:08 +0500

    correction

--- src/commons/auth/LogIn.js
+++ src/commons/auth/LogIn.js
@@ -5,7 +5,6 @@ const LogIn = () => {
         Thank you! You have successfully signed up! Unfortunately, You can not create a blog
         unless the author promotes you as an admin. <a href="https://t.me/Abdunazarov97">Contact the author here!</a>
       </h2>
-
     </div>
   );
 };
