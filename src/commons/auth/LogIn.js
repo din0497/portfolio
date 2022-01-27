ea616c9bd35154fb07e1d6ac952b81b4c7c7dbc6
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-27 09:51:37 +0500

    correction

--- src/commons/auth/LogIn.js
+++ src/commons/auth/LogIn.js
@@ -3,7 +3,7 @@ const LogIn = () => {
     <div style={{width:'81%', margin:'auto', textAlign:'left', marginBottom:'211px'}}>
       <h2>
         Thank you! You have successfully signed up! Unfortunately, You can not create a blog
-        unless the author promote you as an admin. <a href="https://t.me/Abdunazarov97">Contact the author here!</a>
+        unless the author promotes you as an admin. <a href="https://t.me/Abdunazarov97">Contact the author here!</a>
       </h2>
 
     </div>
